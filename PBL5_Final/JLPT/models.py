9e27edb48ee84a9467cb894eec7af660b8831d12
holequoctin <holequocti@gmail.com>
2023-04-25 10:03:34 +0700

    authen

--- PBL5_Final/JLPT/models.py
+++ PBL5_Final/JLPT/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
-    role = models.CharField(max_length=20)
-    phone_number = models.CharField(max_length=11)
+    role = models.CharField(max_length=20,default="student")
+    phone_number = models.CharField(max_length=11,default='')
     user = models.OneToOneField(User,on_delete=models.CASCADE)
