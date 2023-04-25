726052064f9d6aecce3a5a73fdcc1d6506db5b01
holequoctin <holequocti@gmail.com>
2023-04-25 09:08:23 +0700

    add profile model

--- PBL5_Final/JLPT/models.py
+++ PBL5_Final/JLPT/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    role = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=11)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
