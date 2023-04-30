540db06dddbd2787cdfabf740b1cae1a3a31645f
holequoctin <holequocti@gmail.com>
2023-04-30 11:00:42 +0700

    update exam

--- PBL5_Final/Exam/views.py
+++ PBL5_Final/Exam/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect,get_list_or_404,get_object_or_404
 from .models import Exam
 from .forms import *
+from django.core.files.storage import default_storage
 # Create your views here.
 
 def detail_exam(request,pk):
@@ -9,8 +10,16 @@ def detail_exam(request,pk):
     return render(request,'partials/exam_detail.html',{'form':form})
 
 def edit_exam(request,pk):
-    if request.method == "POST":
-        pass
     exam = get_object_or_404(Exam,id = pk)
+    if request.method == "POST":
+        print("POST")
+        form = AddExamForm(request.POST, request.FILES,instance = exam)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+            return redirect('Teacher:DetailExam',pk=form.instance.id)
+        else:
+            print("Form is not valid")
+            print(form.cleaned_data)
     form = AddExamForm(instance=exam)
     return render(request,'partials/edit_exam_form.html',{'form' : form})
